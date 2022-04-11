9fbf7b3b12ea4e54a46b799efb40c24ca2768247
ChenYiran <37398747+AsherJingkongChen@users.noreply.github.com>
2022-04-11 23:20:43 +0800

    Update error.h

--- CpplusPractice/p1/error.h
+++ CpplusPractice/p1/error.h
@@ -2,17 +2,19 @@
 #define CIIS_ERROR_H
 
 #include <stdexcept>
+#include <string>
 
 class error : public std::exception {
 private:
     std::string error_message;
 public:
-    error(const std::string& emsg) {
+    error(const char* emsg)
+    {
         error_message = emsg;
     }
-    const char* what() const noexcept {
-        return error_message.c_str();
-    }
+
+    const char* what() const noexcept override {
+    return error_message.c_str(); }
 };
 
 #endif //CIIS_ERROR_H
