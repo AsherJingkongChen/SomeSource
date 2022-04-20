59d391e0e9999ca9bfa8cd179e5a4d6856302de3
ChenYiran <37398747+AsherJingkongChen@users.noreply.github.com>
2022-04-21 01:20:59 +0800

    Update getlist.py

--- PythonPractice/p1/getlist.py
+++ PythonPractice/p1/getlist.py
@@ -1,11 +1,13 @@
 def getlist(original_list):
 	created_list = []
-	bound = len(original_list) // 2 + (len(original_list) & 1)
-	while len(original_list) != 0:
-		for offset in range(bound):
-			created_list.append(original_list[offset])
-			del original_list[offset]
-		bound = len(original_list) // 2 + (len(original_list) & 1)
+	o_length = len(original_list)
+	bound = o_length // 2 + (o_length & 1)
+	while o_length != 0:
+		for get in range(bound):
+			created_list.append(original_list[get])
+			del original_list[get]
+		o_length -= bound
+		bound = o_length // 2 + (o_length & 1)
 	return created_list
 if __name__ == '__main__':
-	print(getlist( [1,2,3,4,5,6] ))
+	print(getlist([1,2,3,4,5,6]))
