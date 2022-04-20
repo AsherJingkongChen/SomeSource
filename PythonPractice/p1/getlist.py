d254da5dcbe5ec83aea47c72d9e2385e7d721f00
ChenYiran <37398747+AsherJingkongChen@users.noreply.github.com>
2022-04-21 00:57:30 +0800

    Create getlist.py

---
+++ PythonPractice/p1/getlist.py
@@ -0,0 +1,10 @@
+def getlist(original_list):
+	created_list = []
+	bound = len(original_list) // 2 + (len(original_list) & 1)
+	while len(original_list) != 0:
+		for offset in range(bound):
+			created_list.append(original_list[offset])
+			del original_list[offset]
+		bound = len(original_list) // 2 + (len(original_list) & 1)
+	return created_list
+print(getlist([1,2,3,4,5,6]))
