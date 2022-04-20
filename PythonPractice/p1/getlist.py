fada6b97846a731fe14507a63c5d47f32419e248
ChenYiran <37398747+AsherJingkongChen@users.noreply.github.com>
2022-04-21 01:01:12 +0800

    Update getlist.py

--- PythonPractice/p1/getlist.py
+++ PythonPractice/p1/getlist.py
@@ -7,4 +7,5 @@ def getlist(original_list):
 			del original_list[offset]
 		bound = len(original_list) // 2 + (len(original_list) & 1)
 	return created_list
-print(getlist([1,2,3,4,5,6]))
+if __name__ == '__main__':
+	print(getlist( [1,2,3,4,5,6] ))
