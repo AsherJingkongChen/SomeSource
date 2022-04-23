60a05f696fcfd7a565c297fcb13ad4d6c81e8b5f
ChenYiran <37398747+AsherJingkongChen@users.noreply.github.com>
2022-04-24 04:09:26 +0800

    Create empty_class.h

---
+++ CpplusPractice/design/rule_of_five/empty_class.h
@@ -0,0 +1,82 @@
+/**
+ * Ciis' Cplusplus class rule of "2 + basic 6 + custom"
+ * _1. private members
+ * _2. public members + get + set
+ * b1. destructor
+ * b2. constructors 
+ * b3. copy constructor
+ * b4. move constructor
+ * b5. copy assignment operator
+ * b6. move assignment operator
+ * _3. custom methods
+ */
+
+#ifndef CIIS_EMPTYCLASS_H
+#define CIIS_EMPTYCLASS_H
+
+/* inclusion */
+
+class empty_class {
+private:
+/* members */
+    void* ptr;
+
+public:
+/* get & set */
+    void* 
+    bond;
+    
+    void*
+    get_ptr() const
+    {
+        return ptr;
+    }
+
+// basic method section :
+
+/* dtor */
+    ~empty_class() = delete;
+
+/* ctor0 */
+    empty_class()
+        : ptr(nullptr)
+        , bond(nullptr) {}
+
+/* ctorA */
+    empty_class(size_t _size)
+        : ptr(new char[_size])
+        , bond(nullptr) {}
+  
+/* cpctor */
+    empty_class(const empty_class& _src)     {};
+  
+/* mvctor */
+    empty_class(empty_class&& _src) noexcept {};
+  
+/* copy = */
+    empty_class&
+    operator=(const empty_class& _rhs)       {};
+  
+/* move = */
+    empty_class&
+    operator=(empty_class&& _rhs)   noexcept {};
+  
+// custom method section :
+  
+/* assert equivalence */
+    bool
+    operator==(const empty_class& _rhs) const
+    {
+        return false; 
+    }
+  
+/* assert equivalence */
+    bool
+    operator!=(const empty_class& _rhs) const
+    {
+        return !(*this == _rhs); 
+    }
+
+};
+
+#endif //CIIS_EMPTYCLASS_H
