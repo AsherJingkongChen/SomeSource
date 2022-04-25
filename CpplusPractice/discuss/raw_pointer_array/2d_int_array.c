3477e30fb57547d7832392b84d9a5a951d0bafce
ChenYiran <37398747+AsherJingkongChen@users.noreply.github.com>
2022-04-25 21:14:56 +0800

    Create 2d_int_array.c

---
+++ CpplusPractice/discuss/raw_pointer_array/2d_int_array.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+int main() 
+{
+    const uint64_t BigLength    = 5;
+    const uint64_t BigSize      = sizeof(int*);
+    const uint64_t SmallLength  = 11;
+    const uint64_t SmallSize    = sizeof(int);
+
+    // pointer_2 = pointer_2;
+  
+    int** big = (int**) malloc(BigLength * BigSize); 
+
+    for (uint32_t i = BigLength; i--;)
+    {
+        // pointer_1 = pointer_1;
+      
+        *(big + i) = (int*) malloc(SmallLength * SmallSize);
+        
+        for (int j = SmallLength; j--;)
+        {
+            // *(pointer_2 + n) = pointer_1; 
+            // *(pointer_1 + n) = int;
+          
+            *(*(big + i) + j) = j * 3; 
+        }
+    }
+
+    for (uint32_t i = 0; i < BigLength; i++)
+    {
+        for (uint32_t j = 0; j < SmallLength; j++)
+        {
+            // *(pointer_2 + n) = pointer_1; 
+            // *(pointer_1 + n) = int;
+          
+            printf("%d ", *(*(big + i) + j));
+        }
+      
+        // *(pointer_2 + n) = pointer_1 [heap allocated];
+      
+        free(*(big + i));
+        printf("\n");
+    }
+  
+    // pointer_2 [heap allocated]
+  
+    free(big);
+
+    return 0;
+}
