f00a97206df247b1dedd76b441d6e56c0e4d0778
ChenYiran <37398747+AsherJingkongChen@users.noreply.github.com>
2022-05-02 23:20:43 +0800

    Create strict_component.h

---
+++ CpplusPractice/design/rule_of_five/strict_component.h
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+template<class TYPE>
+class lock_pair {
+private:
+
+// internal method :
+
+public:
+// basic method :
+
+// custom method :
+
+// any copy and move operation is not recommended :
+    lock_pair(const lock_pair&)         = delete;
+    lock_pair(lock_pair&&)              = delete;
+    void operator=(const lock_pair&)    = delete;
+    void operator=(lock_pair&&)         = delete;
+};
